540241b19727ec45479af979ce5e8e690f7e237b
Mark Bykhovets <MBykhovets@beeline.ru>
2021-03-11 20:47:01 +0300

    tests done

--- src/Order/Order.test.js
+++ src/Order/Order.test.js
@@ -1,8 +1,53 @@
 import React from 'react'
+import {shallow, configure} from 'enzyme';
+import Adapter from "enzyme-adapter-react-16";
+import Order from "./Order";
+import {fakeOrders} from "../data/fakeOrders";
+
+jest.mock('../utils/getDate');
+/* eslint-disable import/first */
+import {getDate} from '../utils/getDate';
+
+configure({adapter: new Adapter()});
 
 describe('Order.js', () => {
-  it('some test', () => {
+    getDate.mockReturnValue('11 марта, чт, 2021 год');
 
-  });
+    it('right render', () => {
+        const wrapper = shallow(<Order order={fakeOrders[0]}/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('render without items', () => {
+        const wrapper = shallow(<Order order={
+            {
+                id: 123,
+                date: 1544356800000,
+                shop: 'Alihandro Express',
+            }
+        }/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('render without shop', () => {
+        const wrapper = shallow(<Order order={
+            {
+                id: 1,
+                date: 1544356800000,
+                items: [
+                    'item1',
+                    'item2'
+                ]
+            }
+        }/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('render without order', () => {
+        const wrapper = shallow(<Order/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    jest.resetModules();
 });
 
--- src/utils/sortOrders.test.js
+++ src/utils/sortOrders.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {sortByItemCount} from './sortOrders';
+import {sortByItemCount, getSortFunction, sortByDate, sortOrders, sortTypes} from './sortOrders';
+
 
 describe('sortByItemCount function', () => {
 	it('orders are null', () => {
@@ -20,5 +21,131 @@ describe('sortByItemCount function', () => {
 
 		expect(result).toBe(0);
 	});
+
+	it('right sort: left < right', () => {
+		const order1 = {
+			items: ['item1', 'item2'],
+		};
+
+		const order2 = {
+			items: ['item1', 'item2', 'item3'],
+		};
+
+		expect(sortByItemCount(order1, order2)).toBe(-1);
+	});
+
+	it('right sort: left > right', () => {
+		const order1 = {
+			items: ['item1', 'item2', 'item3'],
+		};
+
+		const order2 = {
+			items: ['item1', 'item2'],
+		};
+
+		expect(sortByItemCount(order1, order2)).toBe(1);
+	});
+
+	it('not objects', () => {
+		expect(sortByItemCount(NaN, NaN)).toBe(0);
+	});
+
+	it('empty objects', () => {
+		expect(sortByItemCount({}, {})).toBe(0);
+	});
+
+	it('without items', () => {
+		expect(sortByItemCount({}, {items: ['1']})).toBe(0);
+	});
 });
 
+
+describe('getSortFunction', () => {
+	it('right date func', () => {
+		expect(getSortFunction('date')).toBe(sortByDate);
+	});
+
+	it('right count func', () => {
+		expect(getSortFunction('count')).toBe(sortByItemCount);
+	});
+});
+
+describe('sortByDate', () => {
+	it('left < right', () => {
+		const order1 = {
+			date: 1544356800000,
+		};
+
+		const order2 = {
+			date: 1544356900000,
+		};
+
+		expect(sortByDate(order1, order2)).toBe(1);
+	});
+
+	it('left > right', () => {
+		const order1 = {
+			date: 1544356800000,
+		};
+
+		const order2 = {
+			date: 1544356500000,
+		};
+
+		expect(sortByDate(order1, order2)).toBe(-1);
+	});
+
+	it('equal', () => {
+		const order1 = {
+			date: 1544356800000,
+		};
+
+		const order2 = {
+			date: 1544356800000,
+		};
+
+		expect(sortByDate(order1, order2)).toBe(0);
+	});
+
+	it('not objects', () => {
+		expect(sortByDate(NaN, NaN)).toBe(0);
+	});
+
+	it('empty', () => {
+		expect(sortByDate(null, null)).toBe(0);
+	});
+
+	it('no date', () => {
+		expect(sortByDate({}, {date: 1})).toBe(0);
+	});
+
+	it('no args', () => {
+		expect(sortByDate()).toBe(0);
+	});
+});
+
+describe('sortOrders callbacks', ()=> {
+	it('not call callback', () => {
+		const orders = [];
+
+		const clb = jest.fn();
+		sortOrders(orders, clb);
+		expect(clb).not.toHaveBeenCalled();
+	});
+
+	it('call callback', () => {
+		const orders = [
+			{items: ['item1', 'item2']},
+			{items: ['item1', "item2", "item3"]}
+		];
+
+		const clb = jest.fn();
+		sortOrders(orders, clb);
+		expect(clb).toHaveBeenCalled();
+	});
+
+	it('without function', () => {
+		const result = sortOrders([1,2,3], null);
+		expect(result).toBeUndefined();
+	})
+});
